98cf5972bc7b0636a501023f8db89a0b967ff29c
Willams Souza <willamsbernardo@hotmail.com>
2021-07-27 19:55:26 -0300

    Service layer, component registration

---
+++ src/main/java/com/javacompleto/curso/services/UserService.java
@@ -0,0 +1,25 @@
+package com.javacompleto.curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.javacompleto.curso.entitites.User;
+import com.javacompleto.curso.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	public User findById(Long Id) {
+		Optional<User> obj = repository.findById(Id);
+		return obj.get();
+	}
+}
